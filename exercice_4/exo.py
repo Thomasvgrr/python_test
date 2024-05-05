9207c785f0aa8d40b8e4cb26c28abcbae89b700b
ThomasHawwk <165882223+ThomasHawwk@users.noreply.github.com>
2024-05-05 15:50:01 +0200

    end exo 4

---
+++ exercice_4/exo.py
@@ -0,0 +1,13 @@
+fruits =["pomme", "banane", "orange"]
+
+fruits.append ("kiwi")
+
+fruits.remove ("orange")
+
+fruits[1] = "ananas"
+
+
+print ("la liste de fruits contient", len(fruits), "elements")
+
+fruits.sort()
+print(fruits)
