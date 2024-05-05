1ed3a097670fd9be86c6f9952cf02a0b13f3ef7d
ThomasHawwk <165882223+ThomasHawwk@users.noreply.github.com>
2024-05-05 14:56:14 +0200

    end exercice3

---
+++ exercice_3/test.py
@@ -0,0 +1,14 @@
+nom = "Thomas Verger"
+age = 28
+taille = 1,81
+est_etudiant = False
+
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est étudiant: {est_etudiant}")
+
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
