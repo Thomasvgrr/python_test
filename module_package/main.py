4390cdd01aeac5418a04237eef4f8d378ac2d629
ThomasHawwk <165882223+ThomasHawwk@users.noreply.github.com>
2024-05-05 18:28:39 +0200

    end ex import

---
+++ module_package/main.py
@@ -0,0 +1,14 @@
+import operations
+
+
+def main():
+    # Utilisation de la fonction addition
+    resultat_addition = operations.addition(3, 5)
+    print("Résultat de l'addition :", resultat_addition)
+    # Utilisation de la fonction multiplication
+    resultat_multiplication = operations.multiplication(8, 2)
+    print("Résultat de la multiplication :", resultat_multiplication)
+
+
+if __name__ == "__main__":
+    main()
